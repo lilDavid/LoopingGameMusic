54bf8622163abd3844a1fc59d5214a791401a4a1
lil David <1337lilDavid@gmail.com>
2021-09-11 00:18:44 -0500

    Replace filename strings with Path objects

--- loopaudio/convert.py
+++ loopaudio/convert.py
@@ -1,10 +1,10 @@
+import itertools
 import json
 import locale
-import os
 import sys
-from collections.abc import Sequence, Mapping
+from collections.abc import Mapping, Sequence
+from pathlib import Path, PurePath
 from typing import Iterable, Iterator, NamedTuple, Union
-import itertools
 
 import ffmpeg
 import mutagen
@@ -48,24 +48,20 @@ class SongInfo(NamedTuple):
         return next(self.iter_tracks()).url
 
 
-Filename = str
-
-
 def create_song(
-    json_file: Filename,
+    json_file: Union[str, PurePath],
     info: Union[SongInfo, Iterable[SongInfo]]
 ) -> None:
-    create_directory_for_file(json_file)
-    parts = create_song_parts(os.path.splitext(json_file)[0], info)
+    json_path = Path(json_file)
+    create_directory_for_file(json_path)
+    parts = create_song_parts(json_path, info)
     parts = parts[0] if len(parts) == 1 else parts
     json.dump(parts, open(json_file, "w"))
 
 
-def create_directory_for_file(file: Filename) -> None:
+def create_directory_for_file(file: PurePath) -> None:
     try:
-        os.mkdir(os.path.dirname(file))
-    except FileExistsError:
-        pass
+        Path(file).parent.mkdir(exist_ok=True)
     except FileNotFoundError as e:
         raise ValueError(
             'Cannot automatically create more than one directory:\n'
@@ -74,34 +70,34 @@ def create_directory_for_file(file: Filename) -> None:
 
 
 def create_song_parts(
-    local_filename: Filename,
+    file_path: PurePath,
     info: Union[SongInfo, Iterable[SongInfo]],
 ) -> Sequence[Mapping]:
     if isinstance(info, SongInfo):
-        return [create_part(local_filename, info)]
+        return [create_part(file_path, info)]
     else:
-        return [create_part(local_filename, songinfo) for songinfo in info]
+        return [create_part(file_path, songinfo) for songinfo in info]
 
 
 def create_part(
-    local_filename: Filename,
+    file_path: PurePath,
     songinfo: SongInfo
 ) -> Mapping:
     metadata = get_file_information(songinfo)
 
     variant_map, layer_map = download_and_convert_brstms(
-        local_filename,
+        file_path,
         songinfo
     )
-    files = list_track_filenames(local_filename, variant_map, layer_map)
+    files = list_track_filenames(file_path, variant_map, layer_map)
 
-    filename = create_multitrack_file(
-        local_filename,
+    song_file_path = create_multitrack_file(
+        file_path,
         songinfo,
         metadata,
         files
     )
-    add_metadata(metadata, filename)
+    add_metadata(metadata, song_file_path)
 
     return {
         "version": 2,
@@ -157,32 +153,32 @@ def get_metadata_from_table(table: BeautifulSoup) -> Metadata:
 
 
 def download_and_convert_brstms(
-    file_basename: Filename,
+    file_path: PurePath,
     songinfo: SongInfo
 ) -> tuple[Mapping, ...]:
     print('Downloading BRSTM files...')
 
-    variants = download_tracks(file_basename, songinfo.variants)
-    layers = download_tracks(file_basename, songinfo.layers, len(variants))
+    variants = download_tracks(file_path, songinfo.variants)
+    layers = download_tracks(file_path, songinfo.layers, len(variants))
 
     return variants, layers
 
 
 def download_tracks(
-    file_basename: Filename,
+    file_path: PurePath,
     tracklist: Sequence[SongVariantURL],
     start: int = 0
 ) -> Mapping[str, int]:
     track_map = {}
     for i, track in enumerate(tracklist, start):
         soup = open_page(track.url)
-        download_brstm(soup, file_basename)
-        convert_brstm(file_basename, i)
+        download_brstm(soup, file_path)
+        convert_brstm(file_path, i)
         track_map[track.name] = i
     return track_map
 
 
-def download_brstm(soup: BeautifulSoup, filename: Filename) -> None:
+def download_brstm(soup: BeautifulSoup, path: PurePath) -> None:
     soup = soup.find(id="brstmdl")
     soup = soup.find_all("a")[0]
     brstm_link = "https://web.archive.org/" + soup.attrs["href"]
@@ -190,56 +186,50 @@ def download_brstm(soup: BeautifulSoup, filename: Filename) -> None:
     with requests.get(brstm_link, stream=True) as request:
         request.raise_for_status()
 
-        with open(f'{filename}.brstm', "wb") as file:
+        with open(f'{path.parent / path.stem}.brstm', "wb") as file:
             for chunk in request.iter_content(chunk_size=8192):
                 file.write(chunk)
 
 
-def convert_brstm(filename: Filename, number: int) -> Filename:
-    infile = f'{filename}.brstm'
-    outfile = f'{filename}-{number}.flac'
-    ffmpeg.input(infile).output(outfile).run(overwrite_output=True)
-    os.remove(infile)
-    return outfile
+def convert_brstm(path: Path, number: int) -> PurePath:
+    inpath = path.parent / f'{path.stem}.brstm'
+    outpath = path.parent / f'{path.stem}-{number}.flac'
+    ffmpeg.input(str(inpath)).output(str(outpath)).run(overwrite_output=True)
+    inpath.unlink()
+    return outpath
 
 
 def list_track_filenames(
-    file_basename: Filename,
+    file_path: PurePath,
     *tracklists: Mapping[str, int]
 ) -> Sequence[sf.SoundFile]:
     tracklists = map(Mapping.values, tracklists)
     tracklist = itertools.chain.from_iterable(tracklists)
-    return [sf.SoundFile(f'{file_basename}-{n}.flac') for n in tracklist]
+    return [sf.SoundFile(file_path.parent / f'{file_path.stem}-{n}.flac') for n in tracklist]
 
 
 def create_multitrack_file(
-    local_filename: Filename,
+    json_path: PurePath,
     songinfo: SongInfo,
     metadata: Metadata,
     files: Iterable[sf.SoundFile]
-) -> Filename:
-    songfile = create_sound_file(local_filename, songinfo, metadata.samplerate)
+) -> PurePath:
+    song_path = json_path.parent / songinfo.file
+    songfile = create_sound_file(song_path, songinfo, metadata.samplerate)
     merge_sound_files(files, songfile)
     lengthen_file_if_needed(metadata, files, songfile)
-    for file in files:
-        file.close()
-        os.remove(file.name)
-    songfile.close()
+    close_files(files, songfile)
     
-    return songfile.name
+    return song_path
 
 
 def create_sound_file(
-    local_filename: Filename,
+    file_path: PurePath,
     songinfo: SongInfo,
     samplerate: int
 ) -> sf.SoundFile:
-    directory = os.path.dirname(local_filename)
-    if directory:
-        directory += '/'
-
     return sf.SoundFile(
-        directory + songinfo.file,
+        file_path,
         mode='w',
         samplerate=samplerate,
         channels=(len(songinfo.variants) + len(songinfo.layers)) * 2,
@@ -272,6 +262,13 @@ def lengthen_file_if_needed(
         copy_chunk(songfile, files, 2048)
 
 
+def close_files(files_to_remove: Iterable[sf.SoundFile], file_to_not_remove: sf.SoundFile):
+    for file in files_to_remove:
+        file.close()
+        Path(file.name).unlink()
+    file_to_not_remove.close()
+
+
 def copy_chunk(
     output_file: sf.SoundFile,
     input_files: Iterable[sf.SoundFile],
@@ -297,10 +294,10 @@ def read_chunk(
     return data[:maxlength]
 
 
-def add_metadata(metadata: Metadata, filename: Filename) -> None:
+def add_metadata(metadata: Metadata, file_path: PurePath) -> None:
     print('Metadata...')
 
-    tags = mutagen.File(filename)
+    tags = mutagen.File(file_path)
     tags['title'] = [metadata.title]
     if isinstance(metadata.game, str):
         tags['game'] = [metadata.game]
@@ -356,7 +353,7 @@ def main():
 
     file = create_song_parts(local_filename, SongInfo("", variants, layers))
     file["name"] = input(
-        "Enter VGM title (leave blank to use file name): ") or os.path.basename(local_filename)
+        "Enter VGM title (leave blank to use file name): ") # or os.path.basename(local_filename)
     json.dump(
         file,
         open(local_filename + ".json", "wt")
